ec91317d558052696f2885944ebe5192e0261726
Eric G. Wolfe <wolfe21@marshall.edu>
2012-06-02 00:04:45 -0400

    Add Travis CI badge

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "wolfe21@marshall.edu"
 license          "Apache 2.0"
 description      "Installs/Configures gqlplus"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
 %w{ build-essential readline ncurses ark }.each do |cb|
   depends cb
 end
